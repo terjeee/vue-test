4f4a551ed1f5be5f013c8cf0f0154787c72c6bad
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-15 03:56:32 +0100

    :)

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      friends: [{ id: "manuel", name: "Manuel Lorenz", phone: 12341234, email: "manuel@test.com" }],
+    };
+  },
+};
+</script>
+
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+</template>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="showDetails">
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friend: { id: "manuel", name: "Manuel Lorenz", phone: 12341234, email: "manuel@test.com" },
+      showDetails: false,
+    };
+  },
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+  },
+};
+</script>
+np
